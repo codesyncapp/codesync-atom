f0af51fe0accfe955551a3d88159769fd494000a
Basit-qc <basit.qc@gmail.com>
2021-01-03 01:35:57 +0500

    Synced with VS-Code-Plugin

---
+++ spec/codesync-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodesyncAtomView from '../lib/codesync-atom-view';
+
+describe('CodesyncAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
